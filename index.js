609565581baecada9b00958d6fd631bb51e79fd0
priyadarshiu5 <priyadarshiu5@gmail.com>
2026-07-13 13:13:02 +0530

    1st

---
+++ index.js
@@ -0,0 +1,40 @@
+import mongoose from "mongoose";
+import express from "express";
+import cors from "cors";
+import Email from "./models/email.js";
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect("mongodb://localhost:27017/mydb");
+// Local:
+// mongoose.connect("mongodb://localhost:27017/mydb");
+
+app.get("/", (req, res) => {
+    res.json({
+        message: "Hello World"
+    });
+});
+// Save Email
+app.post("/save", async (req, res) => {
+    await Email.create({
+        email: req.body.email
+    });
+
+    res.json({
+        message: "Saved Successfully"
+    });
+});
+
+// Get All Emails
+app.get("/emails", async (req, res) => {
+
+    const emails = await Email.find();
+
+    res.json(emails);
+});
+
+app.listen(3000, () => {
+    console.log("Server Running");
+});
