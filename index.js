e2d003b3ff45e2bd130d3ac5e35d49f0bf1969c7
priyadarshiu5 <priyadarshiu5@gmail.com>
2026-08-16 13:42:35 +0530

    github action workflow cofigured

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
-import mongoose from "mongoose";
+// import mongoose from "mongoose";
 import express from "express";
-import cors from "cors";
+// import cors from "cors";
 import Email from "./models/email.js";
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect("mongodb://localhost:27017/mydb");
+// mongoose.connect("mongodb://localhost:27017/mydb");
 // Local:
 // mongoose.connect("mongodb://localhost:27017/mydb");
 
 app.get("/", (req, res) => {
     res.json({
-        message: "Hello World"
+        message: "Hello World from Action"
     });
 });
 // Save Email
