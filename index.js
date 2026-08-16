3f5a3d704010f07040d92826bff82be9d131b5b7
priyadarshiu5 <priyadarshiu5@gmail.com>
2026-08-16 14:36:21 +0530

    response edited

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use(express.json());
 
 app.get("/", (req, res) => {
     res.json({
-        message: "Hello World from Action"
+        message: "Hello World from Github Actions"
     });
 });
 // Save Email
